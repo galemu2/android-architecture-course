5693c581344c7f9ec3b52f4c1ba02dac1625f264
Girum.A <galemu00@gmail.com>
2020-12-21 13:23:59 -0500

    add UI for detils view

--- app/src/main/java/com/techyourchance/mvc/screens/questiondetails/QuestionDetailsActivity.java
+++ app/src/main/java/com/techyourchance/mvc/screens/questiondetails/QuestionDetailsActivity.java
@@ -2,7 +2,10 @@ package com.techyourchance.mvc.screens.questiondetails;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 
+import com.techyourchance.mvc.R;
 import com.techyourchance.mvc.screens.common.BaseActivity;
 
 public class QuestionDetailsActivity extends BaseActivity {
@@ -13,5 +16,11 @@ public class QuestionDetailsActivity extends BaseActivity {
         Intent intent = new Intent(context, QuestionDetailsActivity.class);
         intent.putExtra(EXTRA_QUESTION_ID, questionId);
         context.startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_details_activity);
     }
 }
